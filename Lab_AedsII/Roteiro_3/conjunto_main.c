6b32c1710cf87d9150f0c05bd9ad556c9e054f29
S-Pz <saviofrancisco150@hotmail.com>
2023-09-21 13:01:56 -0300

    finalization

--- Lab_AedsII/Roteiro_3/conjunto_main.c
+++ Lab_AedsII/Roteiro_3/conjunto_main.c
@@ -50,12 +50,75 @@ int main(){
     endl;
     imprime_conjunto(diferenca2);
     
+    printf("Verificando se o elemento 7 pertence ao conjunto1:");
+    endl;
+    if(pertence_conjunto(conjunto1, 2)){
+        printf("Pertence");
+        endl;
+    }else{
+        printf("Não pertence");
+        endl;
+    }
 
+    printf("Verificando se o elemento 5 pertence ao conjunto2:");
+    endl;
+    if(pertence_conjunto(conjunto2, 5)){
+        printf("Pertence");
+        endl;
+    }else{
+        printf("Não pertence");
+        endl;
+    }
 
+    printf("Maior elemento do conjunto1:");
+    endl;
+    printf("%d", maior_valor(conjunto1));
+    endl;
 
+    printf("Menor elemento do conjunto2:");
+    endl;
+    printf("%d", menor_valor(conjunto2));
+    endl;   
 
+    printf("verificando se os conjuntos são iguais:");
+    endl;
+    if(conjunto_igual(conjunto1, conjunto2)){
+        printf("São iguais");
+        endl;
+    }else{
+        printf("Não são iguais");
+        endl;
+    }
 
+    endl;
+    printf("Verificando se o conjunto1 é vazio:");
+    endl;
+    if(conjunto_vazio(conjunto1)){
+        printf("É vazio");
+        endl;
+    }else{
+        printf("Não é vazio");
+        endl;
+    }
+    endl;
+    printf("Verificando se o conjunto2 é vazio:");
+    endl;
+    if(conjunto_vazio(conjunto2)){
+        printf("É vazio");
+        endl;
+    }else{
+        printf("Não é vazio");
+        endl;
+    }
     
+    endl;
+    printf("Verificando os tamanhos dos vetores:");
+    endl;
+    printf("Tamanho do conjunto1: %d", tamanho_conjunto(conjunto1));
+    endl;
+    printf("Tamanho do conjunto2: %d", tamanho_conjunto(conjunto2));
+    endl;
+
     limpa_conjunto(conjunto1);
     limpa_conjunto(conjunto2);
     limpa_conjunto(uniao);
