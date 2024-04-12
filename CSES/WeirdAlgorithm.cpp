8be8d28a77efce0c7cfcc61211d6d646cf46a394
S-Pz <saviofrancisco150@hotmail.com>
2024-04-12 16:22:06 -0300

    adding new folder

---
+++ CSES/WeirdAlgorithm.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main(){
+
+    long int n;
+    
+    cin >> n;
+    
+    if (n==1){
+        cout << n << " " << endl;
+    }else{
+
+        do{
+            cout << n << " ";
+            
+            if (n%2 == 0){
+                n = n/2; 
+            }else{//Se não ímpar
+                n = (n * 3) + 1;
+            }
+
+        }while(n>1);
+
+        cout << 1 << endl;
+    }
+    
+    return 0;
+}
