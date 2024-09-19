88155152c8d61205ded2c8033385b3e43b0058ee
S-Pz <saviofrancisco150@hotmail.com>
2024-09-18 21:28:46 -0300

    Fixing gitignore

--- codes_in_rust/target/debug/codes_in_rust.d
+++
@@ -1,1 +0,0 @@
-/home/savio_francisco/Documents/Coding-contests/codes_in_rust/target/debug/codes_in_rust: /home/savio_francisco/Documents/Coding-contests/codes_in_rust/src/main.rs
--- codes_in_rust/target/debug/deps/codes_in_rust-5562e0c238d49b7e.d
+++
@@ -1,5 +0,0 @@
-/home/savio_francisco/Documents/Coding-contests/codes_in_rust/target/debug/deps/codes_in_rust-5562e0c238d49b7e: src/main.rs
-
-/home/savio_francisco/Documents/Coding-contests/codes_in_rust/target/debug/deps/codes_in_rust-5562e0c238d49b7e.d: src/main.rs
-
-src/main.rs:
